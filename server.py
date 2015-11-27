8be521af2eafe0b6728ba0eeafc74780d73ea712
Alex Cusack <cusackalex@gmail.com>
2015-11-26 20:08:55 -0800

    wip

--- server.py
+++ server.py
@@ -18,28 +18,32 @@ payment = Payment(app, wallet)
 @app.route('/bet')
 @payment.required(1000)
 def get_bet():
+    p request
     return "you're better number X, once 10 betters have played, a winner will be chosen"
 
 # machine-payable endpoint that pays user if answer is correct
-# @app.route('/play')
-# @payment.required(1000)
-# def answer_question():
+@app.route('/play')
+@payment.required(1000)
+def answer_question():
 
-#     # extract answer from client request
-#     answer = request.args.get('selection')
+    # extract answer from client request
+    answer = request.args.get('selection')
 
-#     # extract payout address from client address
-#     client_payout_addr = request.args.get('payout_address')
+    # extract payout address from client address
+    client_payout_addr = request.args.get('payout_address')
 
-#     # extract question from client request
-#     client_question = request.args.get('question')
+    # extract question from client request
+    client_question = request.args.get('question')
 
-#     # check if answer is correct
-#     if answer.lower() == question_bank[client_question].lower():
-#         txid = wallet.send_to(client_payout_addr, 2000)
-#         return "Correct!"
-#     else:
-#         return "Incorrect response."
+    # check if answer is correct
+    if answer.lower() == question_bank[client_question].lower():
+        txid = wallet.send_to(client_payout_addr, 2000)
+        return "Correct!"
+    else:
+        return "Incorrect response."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
+
+
+
