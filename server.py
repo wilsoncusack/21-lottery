1eddb54ae613acc2c53339c2614ecc923afd14ba
Alex Cusack <cusackalex@gmail.com>
2015-11-27 11:13:36 -0800

    fixing query selection

--- server.py
+++ server.py
@@ -22,22 +22,21 @@ payment = Payment(app, wallet)
 def placeBet():
     print("in route")
     dataBaseConnection = psycopg2.connect(database="lottery3", user="twenty", password="md556eb55a1978f8a1a6a7149914d371379")
-    connection = dataBaseConnection.cursor()
-    print("connected to DB")
-    # betsTableConnection = psycopg2.connect(database="bets", user="twenty", password="md556eb55a1978f8a1a6a7149914d371379")
-    # betsTable = betsTableConnection.cursor()
-    # roundsTableConnection = psycopg2.connect(database="bets", user="twenty", password="md556eb55a1978f8a1a6a7149914d371379")
-    # roundsTable = roundsTableConnection.cursor()
+    cursor = dataBaseConnection.cursor()
 
-    winningBetNumber = connection.execute("SELECT winvalue from rounds ORDER BY id DESC;")
-    print("first query")
-    betCount = connection.execute("SELECT max(id) FROM bets;")
-    print("second query")
+    cursor.execute("SELECT winvalue from rounds ORDER BY id DESC;")
+    winningBetNumber = cursor.fetchone()[0]
+    cursor.execute("SELECT max(id) FROM bets;")
+    betCount = cursor.fetchone()[0]
+
+    print("conections and queries success")
+    print(betCount)
+    print(winningBetNumber)
     if betCount + 1 == winningBetNumber:
         print("in if")
-        connection.execute("SELECT pot_size FROM lottery;")
+        cursor.execute("SELECT pot_size FROM lottery;")
         winningAddresss = request.args.get('payout_address')
-        winningPotAmount = connection.execute("SELECT win_size from rounds ORDER BY id DESC;")
+        winningPotAmount = cursor.execute("SELECT win_size from rounds ORDER BY id DESC;")
 
         print("pot size = " + str(winningPotAmount))
         print(winningAddresss)
@@ -51,15 +50,15 @@ def placeBet():
         # roundSize = lastroundSize * 2
         # newWinningBetNumber = getWinningBetNumberFromRange()
         ######################
-        connection.close()
+        cursor.close()
         # roundsTable.close()
 
         return "You win!"
     else:
         print("in else statement")
-        connection.execute("INSERT INTO bets (addresss) VALUES (" + request.args.get('payout_address') + ");")
+        cursor.execute("INSERT INTO bets (addresss) VALUES (" + request.args.get('payout_address') + ");")
 
-        connection.close()
+        cursor.close()
         # roundsTable.close()
 
         return "Sorry! Try again!"
