7ddad46db646a262bdd3972c4f2d044395e9e017
Alex Cusack <cusackalex@gmail.com>
2015-11-26 19:36:10 -0800

    add payment of 1000 s to server at /bet

--- server.py
+++ server.py
@@ -15,30 +15,31 @@ wallet = Wallet()
 payment = Payment(app, wallet)
 
 # endpoint to get a question from the server
-@app.route('/')
+@app.route('/bet')
+@payment.required(1000)
 def get_bet():
     return "you're better number X, once 10 betters have played, a winner will be chosen"
 
 # machine-payable endpoint that pays user if answer is correct
-@app.route('/play')
-@payment.required(1000)
-def answer_question():
+# @app.route('/play')
+# @payment.required(1000)
+# def answer_question():
 
-    # extract answer from client request
-    answer = request.args.get('selection')
+#     # extract answer from client request
+#     answer = request.args.get('selection')
 
-    # extract payout address from client address
-    client_payout_addr = request.args.get('payout_address')
+#     # extract payout address from client address
+#     client_payout_addr = request.args.get('payout_address')
 
-    # extract question from client request
-    client_question = request.args.get('question')
+#     # extract question from client request
+#     client_question = request.args.get('question')
 
-    # check if answer is correct
-    if answer.lower() == question_bank[client_question].lower():
-        txid = wallet.send_to(client_payout_addr, 2000)
-        return "Correct!"
-    else:
-        return "Incorrect response."
+#     # check if answer is correct
+#     if answer.lower() == question_bank[client_question].lower():
+#         txid = wallet.send_to(client_payout_addr, 2000)
+#         return "Correct!"
+#     else:
+#         return "Incorrect response."
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
