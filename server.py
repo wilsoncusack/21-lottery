4847b091951efaaec346aadd450f7fed0139b41b
Wilson <cusackw@gmail.com>
2015-11-28 01:03:57 -0500

    started changing server to match new db setup

--- server.py
+++ server.py
@@ -18,11 +18,6 @@ payment = Payment(app, wallet)
 
 # endpoint to get a question from the server
 #@app.route('/lotterMe')
-def update_count(newCount, cursor):
-    print("in count!!")
-    SQL = "UPDATE lottery SET request_count = %s;"
-    data = (newCount,)
-    cursor.execute(SQL, data)
 
 # machine-payable endpoint that pays user if answer is correct
 @app.route('/lotterMe')
@@ -31,14 +26,15 @@ def answer_question():
     conn = psycopg2.connect(database="lottery3", user="twenty", password="md556eb55a1978f8a1a6a7149914d371379")
     cursor = conn.cursor()
 
-    cursor.execute("SELECT request_count FROM lottery;")
-    count = cursor.fetchone()[0]
-    print("count = " + str(count))
+    client_payout_addr = request.args.get('payout_address')
+    cursor.execute("INSERT INTO bids (address) VALUES (%s);", client_payout_addr)
+    client_bid_number = cursor.fetchone()[0]
+    print(client_bid_number)
 
-    #cursor.execute("SELECT request_count FROM lottery;")
-    #iteration = cursor.fetchone()[0]
+    cursor.execute("SELECT winning_bid_number FROM rounds ORDER BY round_number desc LIMIT 1;")
+    winning_bid_number = cursor.fetchone()[0]
 
-    if (count + 1) == 3:
+    if winning_bid_number == client_bid_number:
         cursor.execute("SELECT pot_size FROM lottery;")
         potSize = int(cursor.fetchone()[0])
         print("pot size = " + str(potSize))
@@ -47,21 +43,18 @@ def answer_question():
 
         client_payout_addr = request.args.get('payout_address')
         print("did you get here?")
-        txid = wallet.send_to(client_payout_addr, potSize)
+        #txid = wallet.send_to(client_payout_addr, potSize)
         print("got here")
         SQL = "UPDATE lottery SET pot_size = %s;"
         newPotSize = (potSize/2) + 3000
         data = (newPotSize,)
         cursor.execute(SQL, data)
-        update_count(0, cursor)
 
         conn.commit()
         cursor.close() 
 
         return "You win!"
     else:
-        update_count((count + 1), cursor)
-
         conn.commit()
         cursor.close() 
 
