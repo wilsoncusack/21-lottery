e25e72bb0e3cabd56a1800b6d448a5006c6642d0
Wilson <cusackw@gmail.com>
2015-11-29 03:13:22 -0500

    code cleanup

--- server.py
+++ server.py
@@ -27,25 +27,17 @@ def view():
     # can just grab the data here
     cursor.execute("SELECT * FROM rounds ORDER BY round_number asc;") # need to modify to not return the most recent
     result = cursor.fetchall()
-    print(result)
     length = len(result)
     data = [i for i in range(length)]
     for d in result:
-        # rounds should start from zero in the future
-        print('up here')
         data[d[0] - 1] = [d[0], d[1], d[2]]
-        print('down here')
-        #data[d[0] - 1] = dict(round_number = d[0], winning_bid_number = d[1], pot_size = d[2])
-        #data[d[0] - 1] = flask.jsonify(**data[d[0] - 1])
-        #data[d[0] - 1] = {"round_number": d[0], "winning_bid_number": d[1], "pot_size": d[2]}
     data[length - 1][1] = -1
-    print(data)
     return render_template('index.html', data=data)
 
-# machine-payable endpoint that pays user if answer is correct
+# machine-payable endpoint that pays user if they are the winning request number
 @app.route('/lotterMe')
-@payment.required(200)
-def answer_question():
+@payment.required(1000)
+def lottery():
     conn = psycopg2.connect(database="lottery3", user="twenty", password="md556eb55a1978f8a1a6a7149914d371379")
     cursor = conn.cursor()
 
@@ -57,10 +49,8 @@ def answer_question():
     cursor.execute("SELECT round_number, winning_bid_number, pot_size FROM rounds ORDER BY round_number desc LIMIT 1;")
     [current_round_number, winning_bid_number, pot_size] = cursor.fetchone()
 
-    print(pot_size)
-
     if winning_bid_number == client_bid_number:
-        #txid = wallet.send_to(client_payout_addr, pot_size/2)
+        txid = wallet.send_to(client_payout_addr, pot_size/2)
 
         cursor.execute("UPDATE bids SET is_winner = TRUE WHERE id = %s;", (client_bid_number,))
 
