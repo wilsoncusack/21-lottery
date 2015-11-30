caa0c0044d30dc27734b9ae907006e63d3196885
Wilson <cusackw@gmail.com>
2015-11-30 14:06:58 -0500

    naming is important

--- webview.py
+++ webview.py
@@ -18,7 +18,7 @@ app = Flask(__name__)
 @app.route('/view')
 def view():
 	urlparse.uses_netloc.append("postgres")
-	url = urlparse.urlparse(os.environ["DB_URL"])
+	url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
 	conn = psycopg2.connect(
 	    database=url.path[1:],
